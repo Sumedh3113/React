bf2b2f4395b9d92abf6b53247e013d895ba69a1e
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-14 02:01:14 -0700

    updates

--- Login/client/src/components/Requestor.js
+++ Login/client/src/components/Requestor.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react'
-import { requests } from './UserFunctions'
+import { req1 } from './RequestorFunctions'
 
-class Requestor extends React.Component {
-  constructor(props) {
-    super(props);
+class Requestor extends Component {
+  constructor() {
+    super();
     this.state = {
 //                name: '',
                   required_doc: '',
-                  name:''
+                  name:'',
+                student_id:'',
+                    decrypt:''
                  };
 
-//    this.handleChange1 = this.handleChange1.bind(this);
-    this.handleChange2 = this.handleChange2.bind(this);
-    this.handleChange4 = this.handleChange4.bind(this);
-    
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onChange = this.onChange.bind(this)
+	this.onSubmit = this.onSubmit.bind(this)
+     
+    this.handleClick = this.handleClick.bind(this);
     
   }
 
-//  handleChange1(event) {
-//    this.setState({value1: event.target.value });
-//  }
+  handleChange(event) {
+    this.setState({decrypt: event.target.value });
+  }
 
-handleChange2(event) {
-    this.setState({required_doc: event.target.value });
-  }
- 
-  handleChange4(event) {
-  this.setState({name: event.target.value});
-  }
-  handleSubmit(event) {
+    /*--change this function if want to change decrypt value--*/
+    handleClick(e){
+    //alert("Decrypted Value: " + (this.state.decrypt).slice(1,-3))
+    document.querySelector(".test").textContent = "Decrypted Value: " + (this.state.decrypt).slice(1,-3)
+        
+    }
+	 
     
-//    alert('You selected: ' + this.state.value1);
-//    alert('Name: ' + this.state.name);
-//      alert('Documents: ' + this.state.required_doc);
-      event.preventDefault();
-      
-      const requestor = {
-            name: this.state.name,
-            required_doc: this.state.required_doc        }
+    
+onChange (e) {
+        this.setState({ [e.target.name]: e.target.value,
+                        [e.target.required_doc]:e.target.value,
+                      [e.target.student_id]:e.target.value})
+    }
 
-      
-  }
+    onSubmit (e) {
+        e.preventDefault()
+	//alert("Hello" + this.state.student_id)
+
+        const reqs = {
+			
+            required_doc: this.state.required_doc ,
+			name: this.state.name,
+            student_id:this.state.student_id
+            
+        }
+
+        req1(reqs)
+    }
+    
 
   render() {
     return (
     <div className="container">
         <div className="row">
             <div className="col-md-6 mt-5 mx-auto">
-      <form onSubmit={this.handleSubmit}><br/>
+      <form onSubmit={this.onSubmit}><br/>
         <div className="form-group">
         <label>
           Pick Required Documents: </label>
@@ -65,30 +77,41 @@ handleChange2(event) {
           
           <div className="form-group">
         <label htmlFor="email">Document List:</label>
-    <input type="text" className="form-control" name="doc"
-        placeholder="Enter document names" name="required_doc" value={this.state.required_doc} onChange={this.handleChange2} />
+        <input type="text" className="form-control" 
+        placeholder="Enter document names" name="required_doc" value={this.state.required_doc} onChange={this.onChange} />
                 </div>
           
         <div className="form-group">
           <label>
           Name:</label>
-          <input type="test" placeholder="Enter your name" name="name" className="form-control" value={this.state.name} onChange={this.handleChange4} />
-        <br/>
+          <input type="text" placeholder="Enter your name" name="name" className="form-control" value={this.state.name} onChange={this.onChange} />
+        
             </div>
+          
+        <div className="form-group">
+          <label>
+          Student_ID:</label>
+          <input type="text" placeholder="Enter Users ID" name="student_id" className="form-control" value={this.state.student_id} onChange={this.onChange} />
+        
+            </div>
+
             
-            
-        <input type="submit" value="Send" className="btn btn-lg btn-secondary btn-block active" />
+        <button type="submit" className="btn btn-lg btn-secondary btn-block active" >Send </button>
       </form>
         <br/>        
         <div className="form-group">
           <label>        
               Decrypt :</label> 
-          <input type="text"  placeholder="Enter hash to decrypt" className="form-control" value={this.state.value} onChange={this.handleChange5} />
+          <input type="text"  placeholder="Enter hash to decrypt" className="form-control" name="decypt" value={this.state.decrypt} onChange={this.handleChange} />
         </div>
 
             
-        <input type="button" value="Decrypt" className="btn btn-lg btn-secondary btn-block active"/>
-            
+        <input type="button" onClick={this.handleClick} value="Decrypt" className="btn btn-lg btn-secondary btn-block active"/>
+                <br/>
+            <div class="test">
+                
+                </div>
+                
         </div>
             </div>
             </div>
