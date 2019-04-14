f2202b06d8d5c63c3e8d8861ab33c597c3f2334c
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-14 02:00:11 -0700

    updates

--- Login/models/Request.js
+++ Login/models/Request.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const Request = new Schema({
-    name: {
-        type: String
-    },
+const RequestSchema = new Schema({
+    
     required_doc: {
     type: String
     },
-//    status: {
-//    type: Number,
-//        default:0
-//    },
+	name: {
+        type: String
+    },
+    student_id: {
+    type: String,
+    //    default:0
+    },
     date: {
         type: Date,
         default: Date.now
     }
 })
 
-module.exports = mongoose.model('Request', Request)
+module.exports = Request= mongoose.model('requests', RequestSchema)
 
 
