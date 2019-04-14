baf027ee59946318b0ba5581afe6adc68f5c9517
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-14 02:00:24 -0700

    updates

--- Login/routes/Requestors.js
+++ Login/routes/Requestors.js
@@ -13,15 +13,22 @@ process.env.SECRET_KEY = 'secret'
 
 
 /*------------------New code here---------------------------*/
-requests.post('/requestor', (req, res) => {
+requests.post('/req1', (req, res) => {
     const today = new Date()
     const reqData = {
-        name: req.body.name,
+        
         required_doc: req.body.required_doc,
+		name: req.body.name,
+        student_id:req.body.student_id,
 //        status:req.body.status,
         created: today
     }
-Request.create(reqData)
+//    if(reqData!==""){
+        Request.create(reqData)
+//    }
+	
+})
+
 /*-------------------------Ends here-------------------------*/
 
 module.exports = requests
