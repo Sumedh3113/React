bf28a8f76f0b6c084b92dd762263386e9a6f79cf
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-14 00:42:17 -0700

    supporting page

---
+++ Login/client/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import Requestor from './components/Requestor'
+import Validator from './components/Validator'
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+              <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/requestor" component={Requestor} />
+            <Route exact path="/validator" component={Validator} />
+            <Route exact path="/profile" component={Profile} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
