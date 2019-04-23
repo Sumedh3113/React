c88c683cee62811ae9269c48eb3ae8026e8b6201
Sumedh3113 <sumedhdeshpande31@gmail.com>
2019-04-22 22:52:35 -0700

    main changes in this one

---
+++ Login/NewComponents/components/storehash.js
@@ -0,0 +1,99 @@
+import web3 from './web3';
+//Your contract address
+const address = '0xa30fab0f5a0dda134359e4555df982f514c129b2';
+//Your contract ABI
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "user_Address",
+				"type": "address"
+			}
+		],
+		"name": "getipfs",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			},
+			{
+				"name": "m",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string[]"
+			},
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "userAddress",
+				"type": "address"
+			}
+		],
+		"name": "getIpfsHashByAddress",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string[]"
+			},
+			{
+				"name": "",
+				"type": "string[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "userAddress",
+				"type": "address"
+			}
+		],
+		"name": "hasUser",
+		"outputs": [
+			{
+				"name": "hasIndeed",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
